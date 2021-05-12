424bac3cc7c65b865faaf623f01f929f066175da
matthewrapp <mattrapp25@gmail.com>
2021-05-12 16:02:18 -0600

    Bearer

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config').get(process.env.NODE_ENV);
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.auth;
+    const token = req.headers.authorization.split(' ')[1];
     let userId;
 
     if (!token) {
